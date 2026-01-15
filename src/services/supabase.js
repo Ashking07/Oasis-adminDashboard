11134386ecb6cedde42660374c4931f1a22b93d2
csuftitan <csuftitan@MacBookAir-294.lan>
2026-01-15 12:48:18 -0800

    Upgraded supabase config

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabaseUrl = "https://uceahnvsaaxotaucckmi.supabase.co";
-const supabaseKey = "sb_publishable_pyFLS5GHU2DEzorbnT0bCg_H9z2RhAK";
+export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
